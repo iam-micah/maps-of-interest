86b8f3de6ea5bab0705e9e6e6c0d87c7798f4199
Micah Akai-Nettey <79053404+iam-micah@users.noreply.github.com>
2024-10-11 23:12:14 -0700

    Initial Commit

---
+++ maps-of-interest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  maps-of-interest
+//
+//  Created by Micah Nettey on 10/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
